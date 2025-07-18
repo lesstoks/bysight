dd2dcd5242316ff285ab7eb192a41be05f4fc7bf
Immanuel Valle <immanuel@1ventory.com>
2025-07-18 23:24:26 +0800

    Re-scaffolded api project

--- apps/api/src/app/config/app-config.ts
+++
@@ -1,7 +0,0 @@
-export interface AppConfig {
-  port: number;
-}
-
-export default (): AppConfig => ({
-  port: parseInt(process.env.PORT as string, 10) || 3000,
-});
--- apps/api/src/modules/users/users.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { User } from './entities/users.entity';
-
-@Module({
-  imports: [MikroOrmModule.forFeature([User])]
-})
-export class UsersModule {}
